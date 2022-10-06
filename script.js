6d61b1639d1404712cd54424cedfe4e9b1dda54e
Anatoliegit88 <anatolie.coroban88@gmail.com>
2022-10-06 17:47:10 +0200

    js.treno

---
+++ script.js
@@ -0,0 +1,28 @@
+// Ciao ragazzi,
+// esercizio di oggi: calcolo del prezzo del biglietto del treno
+// cartella/repo js-biglietto-treno
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+// km da percorere
+const percorsoPasseggero = parseInt(
+  prompt("Distanza da percorrere da 1km a 100km")
+);
+// età del passegero
+const etaPasseggero = prompt("Dimmi la tua eta");
+// prezzo base
+const prezzoBase = 0.21;
+// prezzo scontato
+let prezzoBiglietto = " ";
+if (etaPasseggero < 18) {
+  prezzoBiglietto = percorsoPasseggero * prezzoBase * 0.8;
+} else if (etaPasseggero >= 65) {
+  prezzoBiglietto = percorsoPasseggero * prezzoBase * 0.6;
+} else {
+  prezzoBiglietto = percorsoPasseggero * prezzoBase;
+}
+alert(prezzoBiglietto.toFixed(2));
